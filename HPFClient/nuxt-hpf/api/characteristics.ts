2c48dd3fdc1ba69dc1f9e605daf113b4fd4baf4c
PierreMDL <pierresimon.mdl@gmail.com>
2023-06-01 17:43:20 +0000

    correction de fanfictions -> fictions et features -> characteristic dans les segments d'url du frontend

--- HPFClient/nuxt-hpf/api/characteristics.ts
+++ HPFClient/nuxt-hpf/api/characteristics.ts
@@ -2,5 +2,6 @@ import $AxiosWrapper from "~/utils/api";
 import { ICharacteristicFilters } from "@/types/characteristics";
 import { CharacteristicTypeModel, CharacteristicModel } from "~/models/characteristics";
 
-export const getCharacteristics = (filters: ICharacteristicFilters | null): Promise<any> => $AxiosWrapper.get<CharacteristicModel>("/features/features/", filters, CharacteristicModel);
-export const getCharacteristicsTypes = (): Promise<any> => $AxiosWrapper.get<CharacteristicTypeModel>("/features/categories/", null, CharacteristicTypeModel);
+export const getCharacteristics = (filters: ICharacteristicFilters | null): Promise<any> => $AxiosWrapper.get<CharacteristicModel>("/characteristics/characteristics/", filters, CharacteristicModel);
+export const getCharacteristicsTypes = (): Promise<any> => $AxiosWrapper.get<CharacteristicTypeModel>("/characteristics/characteristic-types/", null, CharacteristicTypeModel);
+    
