8b5293ced827c2ee84f6209e4634735c7812d1bd
PierreMDL <pierresimon.mdl@gmail.com>
2025-03-13 20:07:47 +0000

    auto export import map les modèles dans le plugin de sérialisation

---
+++ HPFClient/nuxt-hpf/models/index.ts
@@ -0,0 +1,7 @@
+export * from './users';
+export * from './reviews';
+export * from './characteristics';
+export * from './fanfictions';
+export * from './reviews';
+export * from './themes';
+export * from './news';
--- HPFClient/nuxt-hpf/plugins/serialise.ts
+++ HPFClient/nuxt-hpf/plugins/serialise.ts
@@ -1,26 +1,9 @@
 import { plainToInstance, instanceToPlain } from "class-transformer"
-import { NewsModel } from "~/models/news";
-import { FanfictionModel, TableOfContent } from "~/models/fanfictions";
-import { CharacteristicModel, CharacteristicTypeModel } from "~/models/characteristics";
-import { ReviewModel } from "~/models/fanfictions";
-// import * from "~/models/index.ts";
-
-// const classNameToClass = new Map([
-//   [NewsModel.name, NewsModel]
-// ])
-
+import * as models from "~/models";
 
 export default definePayloadPlugin((nuxtApp) => {
-  definePayloadReducer('NewsModel', data => data && data instanceof NewsModel && instanceToPlain(data))
-  definePayloadReviver('NewsModel', data => plainToInstance(NewsModel, data))
-  definePayloadReducer('FanfictionModel', data => data && data instanceof FanfictionModel && instanceToPlain(data))
-  definePayloadReviver('FanfictionModel', data => plainToInstance(FanfictionModel, data))
-  definePayloadReducer('TableOfContent', data => data && data instanceof TableOfContent && instanceToPlain(data))
-  definePayloadReviver('TableOfContent', data => plainToInstance(TableOfContent, data))
-  definePayloadReducer('CharacteristicModel', data => data && data instanceof CharacteristicModel && instanceToPlain(data))
-  definePayloadReviver('CharacteristicModel', data => plainToInstance(CharacteristicModel, data))
-  definePayloadReducer('CharacteristicTypeModel', data => data && data instanceof CharacteristicTypeModel && instanceToPlain(data))
-  definePayloadReviver('CharacteristicTypeModel', data => plainToInstance(CharacteristicTypeModel, data))
-  definePayloadReducer('ReviewModel', data => data && data instanceof ReviewModel && instanceToPlain(data))
-  definePayloadReviver('ReviewModel', data => plainToInstance(ReviewModel, data))
-})
+  Object.entries(models).forEach(([modelName, modelClass]) => {
+    definePayloadReducer(modelName, data => data && data instanceof modelClass && instanceToPlain(data));
+    definePayloadReviver(modelName, data => plainToInstance(modelClass, data));
+  });
+});
